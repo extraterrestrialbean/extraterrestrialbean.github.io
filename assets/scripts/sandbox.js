d6309cb44176af6b86620cfba0f303e789d1246f
extraterrestrialbean <dhyland012@gmail.com>
2026-01-14 20:15:01 -0800

    Add cookie clicker simulator real

--- assets/scripts/sandbox.js
+++ assets/scripts/sandbox.js
@@ -1,3 +1,10 @@
+let cookieClicks = 0;
+document.getElementById("cookie_clicker").innerHTML = "Clicks: " + cookieClicks;
+
 function buttonThatSaysHi(){
     document.getElementById("hi_text").innerHTML = "hi"
+}
+function incrementCookieClicker(){
+    cookieClicks++;
+    document.getElementById("cookie_clicker").innerHTML = "Clicks: " + cookieClicks;
 }
